dff4dedbf5ad2ff77fa728b7fa1ae48bebf1fca2
brguamanc <byrong1998@gmail.com>
2024-03-18 22:51:30 -0500

    new

---
+++ index.js
@@ -0,0 +1,4 @@
+import app from "./src/app.js";
+import { PORT } from "./src/config.js";
+const puerto = PORT|| 5000
+app.listen(puerto)
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import app from "./app.js";
-import { PORT } from "./config.js";
-const puerto = PORT|| 5000
-app.listen(puerto)
